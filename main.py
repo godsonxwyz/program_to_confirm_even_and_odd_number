7cf281220632f9cf3c3a47a0e5570906ac7bbb73
godsonxwyz <godspowerpatrick502@gmail.com>
2024-01-05 03:21:07 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# this exercise is to practice if an else statement
+
+print("Hello, welcome to the even or odd number verifier!")
+numbers = int(input("Number? "))
+
+remainder = numbers % 2
+
+if remainder >= 1:
+    print("The number you input is an odd number")
+else:
+    print("The number you input is an even number.")
